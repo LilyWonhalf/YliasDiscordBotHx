301013caa2412dc8cb968da09c21429faf644d69
Elian Wonhalf <elianwonhalf@gmail.com>
2017-02-17 18:26:34 +0100

    Fixed help dialog

--- yliasdiscordbothx/model/YliasBaseCommand.hx
+++ yliasdiscordbothx/model/YliasBaseCommand.hx
@@ -1,5 +1,6 @@
 package yliasdiscordbothx.model;
 
+import discordbothx.log.Logger;
 import haxe.PosInfos;
 import yliasdiscordbothx.translations.LangCenter;
 import yliasdiscordbothx.utils.DiscordUtils;
@@ -8,11 +9,13 @@ import discordbothx.service.BaseCommand;
 
 class YliasBaseCommand extends BaseCommand {
     public function new(context: CommunicationContext, ?pos: PosInfos) {
+        super(context);
+
         var serverId = DiscordUtils.getServerIdFromMessage(context.message);
         var commandName = pos.className.substr(pos.className.lastIndexOf('.') + 1).toLowerCase();
 
         description = LangCenter.instance.translate(serverId, Bot.PROJECT_NAME + '.model.commandlist.' + commandName + '.description');
-        super(context);
+        Logger.debug(commandName);
     }
 
     private function l(str: String, ?vars: Array<String>, variant: Int = 0, ?pos: PosInfos) {
