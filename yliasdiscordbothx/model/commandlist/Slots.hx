d4a652f745134031b0631d843fb21fd60578de16
Elian Wonhalf <elianwonhalf@gmail.com>
2017-03-07 17:20:40 +0100

    Added slots command

---
+++ yliasdiscordbothx/model/commandlist/Slots.hx
@@ -0,0 +1,98 @@
+package yliasdiscordbothx.model.commandlist;
+
+import discordhx.message.Message;
+import yliasdiscordbothx.utils.ArrayUtils;
+import discordbothx.core.CommunicationContext;
+
+class Slots extends YliasBaseCommand {
+    public function new(context: CommunicationContext) {
+        super(context);
+    }
+
+    override public function process(args: Array<String>): Void {
+        var emojis: Array<String> = [
+            ':dog:',
+            ':cat:',
+            ':mouse:',
+            ':hamster:',
+            ':rabbit:',
+            ':bear:',
+            ':panda_face:',
+            ':koala:',
+            ':tiger:',
+            ':lion_face:',
+            ':cow:',
+            ':pig:',
+            ':frog:',
+            ':octopus:',
+            ':monkey_face:',
+            ':chicken:',
+            ':penguin:',
+            ':bird:',
+            ':baby_chick:',
+            ':wolf:',
+            ':boar:',
+            ':horse:',
+            ':unicorn:',
+            ':bee:',
+            ':bug:',
+            ':snail:',
+            ':beetle:',
+            ':ant:',
+            ':spider:',
+            ':scorpion:',
+            ':crab:',
+            ':snake:',
+            ':turtle:',
+            ':tropical_fish:',
+            ':fish:',
+            ':blowfish:',
+            ':dolphin:',
+            ':whale:',
+            ':whale2:',
+            ':crocodile:',
+            ':water_buffalo:',
+            ':ox:',
+            ':dromedary_camel:',
+            ':camel:',
+            ':elephant:',
+            ':goat:',
+            ':ram:',
+            ':sheep:',
+            ':rat:',
+            ':turkey:',
+            ':dove:',
+            ':dog2:',
+            ':poodle:',
+            ':dragon_face:',
+            ':eagle:',
+            ':duck:',
+            ':bat:',
+            ':shark:',
+            ':owl:',
+            ':fox:',
+            ':butterfly:',
+            ':deer:',
+            ':gorilla:',
+            ':lizard:',
+            ':rhino:',
+            ':shrimp:',
+            ':squid:'
+        ];
+
+        var first: String = ArrayUtils.random(emojis);
+        var second: String = ArrayUtils.random(emojis);
+        var third: String = ArrayUtils.random(emojis);
+        var answer: String = '';
+
+        if (first == second && second == third) {
+            answer += ':sparkles: **JACKPOT** :sparkles:';
+        } else {
+            answer += ':x: **' + l('try_again').toUpperCase() + '** :x:';
+        }
+
+        context.sendToChannel(first + second + third).then(function (message: Message): Void {
+            context.sendToChannel(answer);
+        });
+    }
+}
